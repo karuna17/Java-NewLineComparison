d1e7354dc2c10da2c2d424c602fc8755b95db3e1
karuna17 <karunakakade8@gmail.com>
2021-05-17 02:40:57 +0530

    Added: Compare lines using compareTo and equals methods

--- LineComparisonDemo.java
+++ LineComparisonDemo.java
@@ -1,36 +1,53 @@
 import java.util.Scanner;
 
 public class LineComparisonDemo {
-	Scanner sc = new Scanner(System.in);
-	public void compareLine() {
+
+	public boolean equals(Object o1,Object o2)
+	{
+		if(o1.equals(o2)){
+			System.out.println("line1 and line2 are equal");
+		}else {
+			System.out.println("line1 and line2 are not equal");
+		}
+		return false;
+		
+	}
+	public static void main(String[] args) {
+		LineComparisonDemo l = new LineComparisonDemo();
+
+		Scanner sc = new Scanner(System.in);
 		System.out.println("Enter the value of X1, X2 ,Y1, Y2 for first line");
 		int x1 = sc.nextInt();
 		int x2 = sc.nextInt();
 		int y1 = sc.nextInt();
 		int y2 = sc.nextInt();
-		
+
 		System.out.println("Enter the value of A1, A2 ,B1, B2 for second line");
 		int a1 = sc.nextInt();
 		int a2 = sc.nextInt();
 		int b1 = sc.nextInt();
 		int b2 = sc.nextInt();
-		double line1 = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (x2 - x1));
-		double line2 = Math.sqrt((a2 - a1) * (a2 - a1) + (a2 - a1) * (b2 - b1));
 
-		System.out.println("Line1: "+line1);
-		System.out.println("Line2: "+line2);
+		int line1 = (int) Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (x2 - x1));
+		int line2 = (int) Math.sqrt((a2 - a1) * (a2 - a1) + (a2 - a1) * (b2 - b1));
 
-		if(line1==line2){
-			System.out.println("Both lines are equal");
-		}else if(line1>line2) {
-			System.out.println("Both line1 is greater than line2");
+		System.out.println("Value of Line1: "+line1);
+		System.out.println("Value of Line2: "+line2);
+
+		Integer i1 = new Integer(line1);
+		Integer i2 = new Integer(line2);
+		int res=i1.compareTo(i2);
+
+		if(res>0){
+			System.out.println("line1 is greater than line2");
+		}else if(res<0) {
+			System.out.println("line1 is less than line2");
 		}else {
-			System.out.println("Both line1 is less than line2");
+			System.out.println("line1 and line2 are equal");
 		}
-	}
-	public static void main(String[] args) {
-     LineComparisonDemo l = new LineComparisonDemo();
-     l.compareLine();
+		
+		System.out.println("=================================");
+		l.equals(i1, i2);		
 	}
 
 }
