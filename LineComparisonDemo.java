69245e37deb78a3a504a02411135805cfea78011
karuna17 <karunakakade8@gmail.com>
2021-05-17 01:55:57 +0530

    Added: check equality of two lengths

--- LineComparisonDemo.java
+++ LineComparisonDemo.java
@@ -1,18 +1,31 @@
 import java.util.Scanner;
 
 public class LineComparisonDemo {
-	Scanner sc=new Scanner(System.in);
+	Scanner sc = new Scanner(System.in);
 	public void compareLine() {
-		System.out.println("Enter the value of X1 and X2");
-		int x1=sc.nextInt();
-		int x2=sc.nextInt();
+		System.out.println("Enter the value of X1, X2 ,Y1, Y2 for first line");
+		int x1 = sc.nextInt();
+		int x2 = sc.nextInt();
+		int y1 = sc.nextInt();
+		int y2 = sc.nextInt();
 		
-		System.out.println("Enter the value of Y1 and Y2");
-		int y1=sc.nextInt();
-		int y2=sc.nextInt();
-		
-		double length=Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(x2-x1));
-		System.out.println("Length of Line: "+length);
+		System.out.println("Enter the value of A1, A2 ,B1, B2 for second line");
+		int a1 = sc.nextInt();
+		int a2 = sc.nextInt();
+		int b1 = sc.nextInt();
+		int b2 = sc.nextInt();
+		double line1 = Math.sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (x2 - x1));
+		double line2 = Math.sqrt((a2 - a1) * (a2 - a1) + (a2 - a1) * (b2 - b1));
+
+		System.out.println("Line1: "+line1);
+		System.out.println("Line2: "+line2);
+
+		if(line1==line2){
+			System.out.println("Both lines are equal");
+		}
+		else{
+			System.out.println("Both lines are not equal");
+		}
 	}
 	public static void main(String[] args) {
      LineComparisonDemo l = new LineComparisonDemo();
