004d8bea9680f275bbe2f65b0c2d464453c29b8f
karuna17 <karunakakade8@gmail.com>
2021-05-17 01:45:35 +0530

    Welcome to line comparison day-5 program

---
+++ LineComparisonDemo.java
@@ -0,0 +1,13 @@
+
+public class LineComparisonDemo {
+
+	public void compareLine() {
+		System.out.println("welcome to Line comparison Day-5");
+	}
+	
+	public static void main(String[] args) {
+     LineComparisonDemo l = new LineComparisonDemo();
+     l.compareLine();
+	}
+
+}
